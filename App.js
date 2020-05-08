2ff51fe711c32d70c6043630df9fde186ebfc1fe
Sandeep Sharma <sandy335582@gmail.com>
2020-05-08 23:09:49 +0530

    design changes in modal

--- App.js
+++ App.js
@@ -1,31 +1,48 @@
 import React, { useState } from "react";
 import { StyleSheet, Button, TextInput, View, Text, Modal } from "react-native";
 import Header from "./components/Header";
-import InputContainer from "./components/InputContainer";
-
 export default function App() {
-  const [text, setText] = useState("");
+  const [itemText, setItemText] = useState("");
   const [isModalVisible, changeModalVisiblity] = useState(false);
-  
+  const [list, setList] = useState([]);
   const handleChange = (item) => {
     setText(item);
   };
 
-  
+  const addItem = () => {
+    setList([
+      ...list,
+      {
+        key: Math.random(),
+        data: itemText,
+      },
+    ]);
+    setItemText("");
+    changeModalVisiblity(false);
+  };
+
+  const openModal = () => {
+    changeModalVisiblity(true);
+  };
+
   return (
     <View style={styles.container}>
       <Header />
 
       <View style={styles.addButton}>
-        <Button  title="Add items" />
+        <Button title="Add items" onPress={openModal} />
       </View>
       <Modal style={styles.modal} visible={isModalVisible}>
-        <TextInput
-          value={text}
-          placeholder="Enter item here"
-          onChangeText={() => handleChange()}
-        />
-        <Button title="Add" />
+        <View style={styles.inputContainer}>
+          <Text>Enter Your Item name here</Text>
+          <TextInput
+            style={styles.input}
+            value={itemText}
+            placeholder="Enter item here"
+            onChangeText={() => handleChange()}
+          />
+          <Button title="Add" onPress={addItem} />
+        </View>
       </Modal>
     </View>
   );
@@ -38,8 +55,22 @@ const styles = StyleSheet.create({
     paddingTop: 40,
   },
   addButton: {
-    padding:10,
+    padding: 10,
     margin: 20,
+  },
+
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    padding: 40,
+  },
+
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    marginVertical:20,
+    padding:10
   },
 
   modal: {
